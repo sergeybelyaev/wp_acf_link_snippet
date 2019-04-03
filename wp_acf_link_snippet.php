33f6d799c37800fcf0239fec378f3df7c2739fe9
Sergey Belyaev <sergeybelya@gmail.com>
2019-04-03 12:58:18 +0300

    Update wp_acf_link_snippet.php

--- wp_acf_link_snippet.php
+++ wp_acf_link_snippet.php
@@ -1,4 +1,4 @@
-<?php if ( $link ) : ?>
+<?php if ( is_array( $link ) && ! empty( $link['url'] ) && ! empty( $link['title'] ) ) : ?>
 	<a class="" href="<?php echo esc_url( $link['url'] ); ?>"<?php if ( $link['target'] ) : ?> target="<?php echo $link['target']; ?>"<?php endif; ?>>
 		<?php echo $link['title']; ?>
 	</a>
